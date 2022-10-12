c433f13392d3f6e8d69ea12bbd9ef21809b534e6
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-12 17:11:28 +0300

    8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,14 @@
 import Player from '@vimeo/player';
-import lodash from 'lodash.throttle';
+import throttle from 'lodash.throttle';
+const LOCAL_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const currentTime = 'videoplayer-current-time';
-player.on('play', function () {
-});
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    console.log(data);
+    localStorage.setItem(LOCAL_KEY, data.seconds);
+  }, 500)
+);
+player.setCurrentTime(localStorage.getItem(LOCAL_KEY) || 0);
