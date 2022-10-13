a7212e5485a7ae86aab351d8e2ff4b0c0a85bd6d
Oleg Gavelya <oleggavelya@gmail.com>
2022-10-13 14:53:48 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,14 +6,19 @@ form.addEventListener('submit', formSubmit);
 form.addEventListener('input', throttle(formInput, 500));
 const VALUE_KEY = 'feedback-form-state';
 const formObject = {};
+const obj = { email: '', massage: '' };
 populateTextarea();
 function formSubmit(evt) {
   evt.preventDefault();
   evt.currentTarget.reset();
   const submitObj = JSON.parse(localStorage.getItem(VALUE_KEY));
-  console.log(submitObj);
+  if (localStorage.getItem(VALUE_KEY)) {
+    console.log(submitObj);
+  } else {
+    console.log(obj);
+  }
+  localStorage.removeItem(VALUE_KEY);
 }
-
 function formInput(evt) {
   formObject[evt.target.name] = evt.target.value;
   const string = JSON.stringify(formObject);
